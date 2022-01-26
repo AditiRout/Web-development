18b9c7643a611b9869396e6e742a2289930eadfb
AditiRout <aditi120000@gmail.com>
2022-01-26 10:57:02 +0530

    changes

---
+++ aditi.js
@@ -0,0 +1,24 @@
+function myFunc(){
+    alert("ready?");
+    document.getElementById("h").innerHTML="We will learn about JavaScript";
+
+}
+console.log("hello world")
+var num1=34;
+var num2=45;
+console.log(num1+num2);
+
+var n1=56;
+var n2=37.8;
+
+var str1="heyoo guys";
+var str2="nice to meet u";
+
+var marks={
+    ravi:56,
+    guha:67,
+    rajan:689
+
+}
+console.log(marks)
+console.log(num1,num2)
