86a9f3ec9d0208fb2ef331c0f6f6966ff27e3306
AditiRout <aditi120000@gmail.com>
2022-01-29 22:22:04 +0530

    changes

--- aditi.js
+++ aditi.js
@@ -22,3 +22,48 @@ var marks={
 }
 console.log(marks)
 console.log(num1,num2)
+
+let myA=["renu",34,true,45]
+myA.push("harry")
+console.log(myA)
+myA.pop();
+console.log(myA)
+// myA.shift()
+// myA.shift()//keeps on shifting
+
+console.log(myA)
+console.log(myA.length)
+console.log(myA.unshift("renu"))//prints the new length
+console.log(myA.toString())
+console.log(myA.sort())
+
+//string methods in javascript
+
+console.log(str1.length)
+console.log(str1.indexOf("guys"))//returns first index for last one use lastIndexOf
+console.log(str1.slice(1,4));
+
+d=str2.replace("nice","good");
+console.log(d,str2)
+
+
+let myDate=new Date();
+console.log(myDate.getTime());
+console.log(myDate.getFullYear());
+console.log(myDate.getDay());
+console.log(myDate.getMinutes());
+console.log(myDate.getHours());
+
+//DOM Manipulation
+let boxElement = document.getElementsByClassName('box');
+        console.log(boxElement);
+
+        boxElement[0].style.width = 100;
+        boxElement[0].style.height = 100;
+        boxElement[0].style.backgroundColor = 'blue';
+
+// let elem=document.getElementsByClassName('box');
+// console.log(elem);
+
+
+// elem[0].style.backgroundColor="yellow";
